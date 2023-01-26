db8ec8fdbfd991fe17434b0c39665487f725ec2e
Mike Soukup <michaeltsoukup@gmail.com>
2023-01-25 20:28:48 -0600

    Base webapp to route to upload page and take a file entry

--- webapp/main.py
+++ webapp/main.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """Main ChatBot HTML"""
+    """Main ChatBot HTML."""
     return render_template("home.html")
+
+@app.route("/upload", methods=['GET','POST'])
+def upload():
+    """Example get image file from post request."""
+    if request.method == "POST":
+        print("Post")
+    else:
+        return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug = True)
