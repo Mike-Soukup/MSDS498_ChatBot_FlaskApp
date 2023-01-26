2601b33a606bbd8bcb490a740194685a9d312d47
Mike Soukup <michaeltsoukup@gmail.com>
2023-01-25 20:30:18 -0600

    black and pydocstyle.

--- webapp/main.py
+++ webapp/main.py
@@ -4,18 +4,21 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def welcome():
-    """Main ChatBot HTML."""
+    """Chatbot API Home Page."""
     return render_template("home.html")
 
-@app.route("/upload", methods=['GET','POST'])
+
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
-    """Example get image file from post request."""
+    """Upload image."""
     if request.method == "POST":
         print("Post")
     else:
         return render_template("upload.html")
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug = True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
