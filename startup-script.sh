fe510923043db9d3dc56a9044c67e1a3f2290109
Mike-Soukup <michaeltsoukup@gmail.com>
2023-02-14 05:11:08 +0000

    Try updated startup script.

--- startup-script.sh
+++ startup-script.sh
@@ -1,3 +1,6 @@
+# Echo commands and fail on error
+set -ev
+
 # Install or update needed software
 sudo apt-get update
 sudo apt-get install -yq git supervisor python python-pip python3-distutils
@@ -8,15 +11,15 @@ export HOME=/root
 git clone git@github.com:Mike-Soukup/MSDS498_ChatBot_FlaskApp.git
 
 # Install Cloud Ops Agent
-sudo bash /MSDS498_ChatBot_FlaskApp/add-google-cloud-ops-agent-repo.sh --also-install
+sudo bash MSDS498_ChatBot_FlaskApp/add-google-cloud-ops-agent-repo.sh --also-install
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3  /MSDS498_ChatBot_FlaskApp
-/bin/bash -c "source  /MSDS498_ChatBot_FlaskApp/env/bin/activate"
- /MSDS498_ChatBot_FlaskApp/env/bin/pip install -r  /MSDS498_ChatBot_FlaskApp/requirements.txt
+virtualenv -p python3 MSDS498_ChatBot_FlaskApp/env
+/bin/bash -c "source MSDS498_ChatBot_FlaskApp/env/bin/activate"
+MSDS498_ChatBot_FlaskApp/env/bin/pip install -r /MSDS498_ChatBot_FlaskApp/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp  
