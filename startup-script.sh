19b47d88921bc1d6b71456256b9d72463230793d
Mike-Soukup <michaeltsoukup@gmail.com>
2023-02-14 04:28:04 +0000

    add python app config.

--- startup-script.sh
+++ startup-script.sh
@@ -8,21 +8,21 @@ export HOME=/root
 git clone git@github.com:Mike-Soukup/MSDS498_ChatBot_FlaskApp.git /opt/app
 
 # Install Cloud Ops Agent
-sudo bash /opt/app/gce/add-google-cloud-ops-agent-repo.sh --also-install
+sudo bash /opt/app/MSDS498_ChatBot_FlaskApp/add-google-cloud-ops-agent-repo.sh --also-install
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3 /opt/app/MSDS498_CHATBOT_FLASKAPP
-/bin/bash -c "source /opt/app/gce/env/bin/activate"
-/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
+virtualenv -p python3 /opt/app/MSDS498_ChatBot_FlaskApp
+/bin/bash -c "source /opt/app/MSDS498_ChatBot_FlaskApp/env/bin/activate"
+/opt/app/MSDS498_ChatBot_FlaskApp/env/bin/pip install -r /opt/app/MSDS498_ChatBot_FlaskApp/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/MSDS498_ChatBot_FlaskApp/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
